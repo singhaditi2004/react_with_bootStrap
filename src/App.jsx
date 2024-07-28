0076bebaa034360b634f6939f6d8f059bcb74ea9
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-07-28 14:32:21 +0530

    new files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  
-
   return (
-    <>
-   
-       
-    </>
-  )
+    <center className="todo">
+      <h1>To-Do App </h1>
+
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-sm-6">
+            <input type="text" name="task" id="task" placeholder="Add a task" />
+          </div>
+          <div className="col-sm-4">
+            <input type="date" id="due_date" placeholder="dd/mm/yy" />
+          </div>
+          <div className="col-sm-2">
+            <button type="button" className="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
